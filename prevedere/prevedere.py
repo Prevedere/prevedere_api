720ab9cb31c6a0d40caac1e4779a984de9a08fe9
Ted Dickinson <tmdickinson@gmail.com>
2018-11-28 15:23:50 -0500

    Addition of type hints, modified documentation for indicator_series()

--- prevedere/prevedere.py
+++ prevedere/prevedere.py
@@ -6,28 +6,32 @@ from pandas.io.json import json_normalize
 class Api:
     API_KEY = ""
 
-    def __init__(self, api_key=API_KEY):
+    def __init__(self, api_key=API_KEY: str):
         self.api_key = api_key
 
-    def fetch(self, path, payload):
+    def fetch(self, path: str, payload: dict) -> dict:
         url = f'https://api.prevedere.com{path}'
         r = requests.get(url, params=payload)
         return r.json()
     
-    def indicator_series(self, provider, provider_id, freq='Monthly', calculation=None, offset=0, verbose=False):
+    def indicator_series(self, provider: str, provider_id: str, freq='Monthly': str, calculation=None: str, offset=0: int, verbose=False: bool) -> df:
         """
         Create a pandas dataframe with data from the API.
+
+        :param provider: Hexidecimal code for a data provider
+        :type provider: str
+
+        :param freq: Frequency of indicator to retrieve ("Annual","SemiAnnual","Quarterly","Monthly","BiWeekly","Weekly","Daily")
+        :type freq: str
         
-        :param url:
-        :type url: string
-        :param freq: Frequency of indicator to retrieve
-        :type freq: string
+        :param calculation: Calculation to transform the indicator ("None","PeriodOverPeriod","YearOverYear","ThreePeriodMoving","FivePeriodMoving","ThreePeriodYearOverYear")
+        :type calculation: string
+
         :param offset: Number of periods to offset
         :type offset: int
-        :param calculation: Calculation to transform the indicator
-        :type calculation: string
+
         :param verbose:
-        :type verbose: boolean
+        :type verbose: bool
         """
         path = f'/indicator/series/{provider}/{provider_id}'
         payload = {'ApiKey': self.api_key,'Frequency': freq, 'Offset': offset, "Calculation": calculation}
@@ -52,40 +56,34 @@ class Api:
 
         return df
 
-    def indicator(self, provider, provider_id):
-        """
-        :param provider:
-        :param provider_id:
-        """
+    def indicator(self, provider: str, provider_id: str) -> df:
         path = f'/indicator/{provider}/{provider_id}'
         payload = {'ApiKey': self.api_key}
         return json_normalize(self.fetch(path, payload)).T
     
-    def providers(self):
+    def providers(self) -> dict:
         path = '/providers'
         payload = {'ApiKey': self.api_key}
         return self.fetch(path,payload)
 
-    def analysis_jobs(self):
+    def analysis_jobs(self) -> dict:
         path = '/analysisjobs'
         payload = {'ApiKey': self.api_key}
         return self.fetch(path,payload)
     
-    def search(self, query):
+    def search(self, query: str) -> df:
         path = '/search'
         payload = {'ApiKey': self.api_key,'Query': query}
         results = self.fetch(path,payload)
         return json_normalize(data = results['Results'])
 
-    def model(self, model_id):
+    def model(self, model_id: str) -> dict:
         path = f'/rawmodel/{model_id}'
         payload = {'ApiKey': self.api_key}
         return self.fetch(path, payload)
 
-
 def main():
     pass
-
 
 if __name__ == '__main__':
     main()
