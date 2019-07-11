6e91d0e2eea73a8cb62472ca6c973050bd47e7e5
Hiram Foster <hfoster@prevedere.com>
2019-07-11 16:46:13 -0400

    print error json

--- prevedere/prevedere.py
+++ prevedere/prevedere.py
@@ -36,6 +36,7 @@ class Api:
             r.raise_for_status()
         except requests.exceptions.HTTPError as errh:
             print("Http Error:", errh)
+            print(r.json())
         except requests.exceptions.ConnectionError as errc:
             print("Error Connecting:", errc)
         except requests.exceptions.Timeout as errt:
